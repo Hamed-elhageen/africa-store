219178c70ef05a8e040b5e7bf53ed78100805b84
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-04-17 22:35:20 +0200

    finished authentication

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+import { UpdatepasswordComponent } from './components/updatepassword/updatepassword.component';
+import { VerificationComponent } from './components/verification/verification.component';
+import { SuccessComponent } from './components/success/success.component';
 
 
 const routes: Routes = [
@@ -18,13 +21,28 @@ const routes: Routes = [
   {
     path: 'forgotpassword',
     component:ForgotpasswordComponent ,
+  },
+  {
+    path: 'updatepassword',
+    component:UpdatepasswordComponent ,
+  },
+  {
+    path: 'verification',
+    component:VerificationComponent ,
+  },
+  {
+    path: 'success',
+    component:SuccessComponent ,
   }
 ];
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
-    ForgotpasswordComponent
+    ForgotpasswordComponent,
+    UpdatepasswordComponent,
+    VerificationComponent,
+    SuccessComponent
   ],
   imports: [
     CommonModule,
