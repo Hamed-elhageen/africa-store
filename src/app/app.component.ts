dbbf65f33d718aede79383c1a66aa9d16d6a6040
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-09-27 22:27:56 +0300

    finish spinner animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Event as RouterEvent } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // ✅ لازم تبقى styleUrls مش styleUrl
 })
 export class AppComponent {
-  title = 'first';
+  title = 'Africa stores';
+
+
+  //for adding spinner animation on each click to route
+  constructor(private router: Router, private spinner: NgxSpinnerService) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationStart) {
+        this.spinner.show();
+      }
+      if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.spinner.hide();
+      }
+    });
+  }
 }
