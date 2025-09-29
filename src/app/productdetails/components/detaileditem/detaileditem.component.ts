a467874524dac727d92eb5904344789324fc5b50
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-09-30 00:33:46 +0300

    finish half product details

--- src/app/productdetails/components/detaileditem/detaileditem.component.ts
+++ src/app/productdetails/components/detaileditem/detaileditem.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../../home/services/products.service';
 
 @Component({
   selector: 'app-detaileditem',
@@ -7,14 +8,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './detaileditem.component.scss'
 })
 export class DetaileditemComponent implements OnInit{
-    currentId:number=0
-constructor(private acitvatedRoute:ActivatedRoute){
+    currentId:number=0;
+    choosedProduct:any;
+constructor(private acitvatedRoute:ActivatedRoute, private productsService : ProductsService){
 
 }
     ngOnInit(): void {
         this.acitvatedRoute.paramMap.subscribe((paramMap)=>{
             this.currentId=Number(paramMap.get('id'))
         })
+
+
+
+        this.choosedProduct =this.productsService.getAll().find((prd:any)=>{
+            return prd.productId===this.currentId;
+        })
     }
+
     // by this when changing the id in the url it will be changed in the currenId , because paramMap is returning observable and iam observing it , when the id changes , change it in the current id
 }
