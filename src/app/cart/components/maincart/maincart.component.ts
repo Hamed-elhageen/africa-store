a97a6873ca975522aa6b5e6538417dc76c64c295
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-09-30 23:43:49 +0300

    finished this day

--- src/app/cart/components/maincart/maincart.component.ts
+++ src/app/cart/components/maincart/maincart.component.ts
@@ -1,109 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-maincart',
   templateUrl: './maincart.component.html',
   styleUrl: './maincart.component.scss'
 })
-export class MaincartComponent {
-cartProducts: any[] = [
-  {
-    productImage: "/cr7-shoes.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
+export class MaincartComponent implements OnInit {
+    constructor(private cartService:CartService){
+
     }
-  },
-  {
-    productImage: "/new-bag.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
+        cartProducts!:any[];
+
+    ngOnInit(): void {
+        this.cartProducts=this.cartService.getAllCartProducts();
     }
-  },
-  {
-    productImage: "/sports-shoes.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
-    }
-  },
-  {
-    productImage: "/new-bag.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
-    }
-  },
-  {
-    productImage: "/sock.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
-    }
-  },
-  {
-    productImage: "/new-bag.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
-    }
-  },
-  {
-    productImage: "/tshirt.png",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
-    }
-  },
-  {
-    productImage: "/arsenal-shirt.svg",
-    title: "Barcelona Bag",
-    description: "Original cotton Barca bag",
-    price: 350,
-    priceBeforeDiscount: 420,
-    size: "M",
-    quantity: 1,
-    get overAllPrice() {
-      return this.price * this.quantity;
-    }
-  }
-];
 
 
 }
