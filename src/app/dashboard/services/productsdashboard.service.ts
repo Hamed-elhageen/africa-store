d91c75b9fd564fba18ea8b202f801402b4219101
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-10-03 12:35:23 +0300

    finish dashboard design

---
+++ src/app/dashboard/services/productsdashboard.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsdashboardService {
+//this service is for all the operations on the producst by the admin like add update , delete and edit
+  constructor() { }
+}
