1f96fff4adda51be3a3b2e542302cb9a879ea07e
Hamed-elhageen <hamedragabelhageen@gmail.com>
2025-11-04 18:45:02 +0200

    updating the problem

--- src/app/dashboard/products/services/productsdashboard.service.ts
+++ src/app/dashboard/products/services/productsdashboard.service.ts
@@ -34,7 +34,7 @@ constructor(private http : HttpClient){ }
 
 headers=new HttpHeaders().set(
                 'Authorization',
-                `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ZjZjMzJlODU2MDBmMDI5MDAwMTQyOSIsImlhdCI6MTc2MTU5ODcyOSwiZXhwIjoxNzYxNjg1MTI5fQ.p01GwzhT2w5nD3sA9I7AJXtNAun_qZ8sKTxWGHAZduc`
+                `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ZjZjMzJlODU2MDBmMDI5MDAwMTQyOSIsImlhdCI6MTc2MjI1NDcyMywiZXhwIjoxNzYyMzQxMTIzfQ.sTq11jL96Ojp77bnM0s85TcgHwtoq9xKF5ZOhQWl11Y`
             )
     addProduct(formData:FormData,catId:string):Observable<any>{
         return this.http.post<any>(`${environment.api}/products/${catId}`,formData,{headers:this.headers}).pipe(
